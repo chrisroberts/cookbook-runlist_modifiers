4d1e0f2c3a8fb4971382890cb32e3181564ec885
Chris Roberts <chrisroberts.code@gmail.com>
2012-05-16 08:32:55 -0700

    Allow automated json creation on nodes

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,3 +5,24 @@ ruby_block "scrub_modifiers" do
     node.delete(:restricted_recipes)
   end
 end
+
+file "/opt/runlist_modifiers.json" do
+  content(
+    JSON.pretty_generate(
+      :allowed_recipes => Array(node[:runlist_modifiers][:allowed_recipes]),
+      :restricted_recipes => Array(node[:runlist_modifiers][:restricted_recipes])
+    )
+  )
+  mode 0644
+  not_if do
+    File.exists?('/opt/runlist_modifiers.json') &&
+    JSON.load(File.read('/opt/runlist_modifiers.json')) == {
+      :allowed_recipes => Array(node[:runlist_modifiers][:allowed_recipes]),
+      :restricted_recipes => Array(node[:runlist_modifiers][:restricted_recipes])
+    }
+  end
+  only_if do
+    node[:runlist_modifiers][:allowed_recipes] ||
+    node[:runlist_modifiers][:restricted_recipes]
+  end
+end
