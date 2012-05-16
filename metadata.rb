a761c6cc19cceba1879902611b9f7150246d3e1e
Chris Roberts <chrisroberts.code@gmail.com>
2012-05-16 08:33:28 -0700

    Version tiny bump

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "chrisroberts.code@gmail.com"
 license          "Apache 2.0"
 description      "Provides run list modifications via node attributes"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.1.3"
 
